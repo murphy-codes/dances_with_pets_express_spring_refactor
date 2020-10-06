c16604f3fa36be10b96e1d6fbef06ea9dc6f3bfa
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-10-06 11:02:25 -0700

    initial commit

---
+++ src/main/java/com/launchacademy/DancesWithPetsExpressSpringRefactorApplication.java
@@ -0,0 +1,11 @@
+package com.launchacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DancesWithPetsExpressSpringRefactorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DancesWithPetsExpressSpringRefactorApplication.class, args);
+	}
+}
