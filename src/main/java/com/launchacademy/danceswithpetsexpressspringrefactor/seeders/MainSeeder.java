361fc7926b2777aaf24c8b54dda582934f4fba6d
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-10-07 22:30:41 -0700

    updated Main (parent) seeder to pass repositories

--- src/main/java/com/launchacademy/danceswithpetsexpressspringrefactor/seeders/MainSeeder.java
+++ src/main/java/com/launchacademy/danceswithpetsexpressspringrefactor/seeders/MainSeeder.java
@@ -1,20 +1,28 @@
 package com.launchacademy.danceswithpetsexpressspringrefactor.seeders;
 
+import com.launchacademy.danceswithpetsexpressspringrefactor.repositories.PetRepository;
+import com.launchacademy.danceswithpetsexpressspringrefactor.repositories.PetTypeRepository;
+import com.launchacademy.danceswithpetsexpressspringrefactor.repositories.AdoptionApplicationRepository;
+import com.launchacademy.danceswithpetsexpressspringrefactor.repositories.SurrenderApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MainSeeder implements CommandLineRunner {
-  @Autowired private AdoptionApplicationSeeder adoptionApplicationSeeder;
+  @Autowired private PetRepository petRepository;
   @Autowired private PetSeeder petSeeder;
+  @Autowired private PetTypeRepository petTypeRepository;
   @Autowired private PetTypeSeeder petTypeSeeder;
+  @Autowired private AdoptionApplicationRepository adoptionApplicationRepository;
+  @Autowired private AdoptionApplicationSeeder adoptionApplicationSeeder;
+  @Autowired private SurrenderApplicationRepository surrenderApplicationRepository;
   @Autowired private SurrenderApplicationSeeder surrenderApplicationSeeder;
 
   @Override public void run(String... args) throws Exception {
-    petTypeSeeder.seed();
-    petSeeder.seed();
-    adoptionApplicationSeeder.seed();
-    surrenderApplicationSeeder.seed();
+    petTypeSeeder.seed(petTypeRepository);
+    petSeeder.seed(petTypeRepository, petRepository);
+    adoptionApplicationSeeder.seed(adoptionApplicationRepository, petRepository);
+    surrenderApplicationSeeder.seed(petTypeRepository, surrenderApplicationRepository);
   }
 }
