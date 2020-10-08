0a13abb8ab49098286a9eac465510bc232f69da4
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-10-07 22:32:45 -0700

    updated AdoptionApplications to receive repositories

--- src/main/java/com/launchacademy/danceswithpetsexpressspringrefactor/seeders/AdoptionApplicationSeeder.java
+++ src/main/java/com/launchacademy/danceswithpetsexpressspringrefactor/seeders/AdoptionApplicationSeeder.java
@@ -5,35 +5,32 @@ import com.launchacademy.danceswithpetsexpressspringrefactor.models.AdoptionAppl
 import com.launchacademy.danceswithpetsexpressspringrefactor.models.Pet;
 import com.launchacademy.danceswithpetsexpressspringrefactor.repositories.AdoptionApplicationRepository;
 import com.launchacademy.danceswithpetsexpressspringrefactor.repositories.PetRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class AdoptionApplicationSeeder {
-  @Autowired private AdoptionApplicationRepository adoptionApplicationRepository;
-  @Autowired private PetRepository petRepository;
+  public void seed(AdoptionApplicationRepository adoptionApplicationRepository, PetRepository petRepository) {
+    if(adoptionApplicationRepository.count() == 0) {
+      List<String> nameList = Arrays.asList("Tom Murphy","Russ Moore","Brendan Montima","Dan Alberts","Patrick Whelan","Monica Gonzalez","Monica Gonzalez","Dan Alberts","Dan Alberts");
+      List<String> phoneNumberList = Arrays.asList("707-555-8666","415-555-7877","617-555-2626","617-555-3286","617-555-7973","805-555-6466","805-555-6466","617-555-3286","617-555-3286");
+      List<String> emailList = Arrays.asList("murphy-codes@github.com","wrmoore@github.com","bhmdev@github.com","danalberts@github.com","prophetsfall@github.com","mandm@cutepetexamples.com","mandm@cutepetexamples.com","danalberts@github.com","danalberts@github.com");
+      List<String> homeStatusList = Arrays.asList("rent","rent","own","rent","own","rent","rent","rent","rent");
+      List<String> applicationStatusList = Arrays.asList("pending","pending","pending","pending","pending","pending","pending","pending","pending");
+      List<Integer> petIdList = Arrays.asList(5,2,1,6,7,3,4,6,8);
+      List<Pet> pets = Lists.newArrayList(petRepository.findAll());
 
-  public void seed() {
-    List<String> nameList = Arrays.asList("Tom Murphy","Russ Moore","Brendan Montima","Dan Alberts","Patrick Whelan","Monica Gonzalez","Monica Gonzalez","Dan Alberts","Dan Alberts");
-    List<String> phoneNumberList = Arrays.asList("707-555-8666","415-555-7877","617-555-2626","617-555-3286","617-555-7973","805-555-6466","805-555-6466","617-555-3286","617-555-3286");
-    List<String> emailList = Arrays.asList("murphy-codes@github.com","wrmoore@github.com","bhmdev@github.com","danalberts@github.com","prophetsfall@github.com","mandm@cutepetexamples.com","mandm@cutepetexamples.com","danalberts@github.com","danalberts@github.com");
-    List<String> homeStatusList = Arrays.asList("rent","rent","own","rent","own","rent","rent","rent","rent");
-    List<String> applicationStatusList = Arrays.asList("pending","pending","pending","pending","pending","pending","pending","pending","pending");
-    List<Integer> petIdList = Arrays.asList(5,2,1,6,7,3,4,6,8);
-    List<Pet> pets = Lists.newArrayList(petRepository.findAll());
-
-    for (int i = 0; i < nameList.size(); i++) {
-      AdoptionApplication adoptionApplication = new AdoptionApplication();
-      adoptionApplication.setName(nameList.get(i));
-      adoptionApplication.setPhoneNumber(phoneNumberList.get(i));
-      adoptionApplication.setEmail(emailList.get(i));
-      adoptionApplication.setHomeStatus(homeStatusList.get(i));
-      adoptionApplication.setApplicationStatus(applicationStatusList.get(i));
-      adoptionApplication.setPet(pets.get(petIdList.get(i)-1));
-      adoptionApplicationRepository.save(adoptionApplication);
+      for (int i = 0; i < nameList.size(); i++) {
+        AdoptionApplication adoptionApplication = new AdoptionApplication();
+        adoptionApplication.setName(nameList.get(i));
+        adoptionApplication.setPhoneNumber(phoneNumberList.get(i));
+        adoptionApplication.setEmail(emailList.get(i));
+        adoptionApplication.setHomeStatus(homeStatusList.get(i));
+        adoptionApplication.setApplicationStatus(applicationStatusList.get(i));
+        adoptionApplication.setPet(pets.get(petIdList.get(i)-1));
+        adoptionApplicationRepository.save(adoptionApplication);
+      }
     }
   }
 }
-
